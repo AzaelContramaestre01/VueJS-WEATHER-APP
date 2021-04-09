21963e45ac2bb5573a8ff6d73759fa9704c64ac7
Azael Contramaestre <galaxyhhd@hotmail.com>
2021-04-09 18:59:54 -0300

    Vue is awesome

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 15 ? 'warm' : ''">
     <main>
       <div class="search-box">
         <input type="text" 
@@ -10,14 +10,14 @@
         @keypress="fetchWeather"
         />
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main !='undefined'">
         <div class="location-box">
-          <div class="location">Punta Arenas, Chile</div>
-          <div class="date">Jueves 15, Marzo, 2021</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">5 C°</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -37,13 +37,59 @@ export default {
     }
   },
   methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults(results){
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = [
+      "January", 
+      "February", 
+      "March", 
+      "April" , 
+      "May", 
+      "June", 
+      "July", 
+      "August", 
+      "September", 
+      "October", 
+      "November", 
+      "December"];
 
+
+      let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()]
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+
+    }
   }
 }
 
 </script>
 
 <style>
+
+
 *{
   margin: 0;
   padding: 0;
@@ -61,6 +107,10 @@ body {
   transition: 0.4s;
 }
 
+#app.warm {
+  background-image: url('./assets/fire.jpg');
+}
+
 main {
   min-height: 100vh;
   padding: 25px;
@@ -70,7 +120,7 @@ main {
 
 .search-box{
   width: 100%;
-  margin-bottom: 30px;
+  margin: 0 0 75px;
 }
 
 .search-box .search-bar {
@@ -100,20 +150,20 @@ main {
 
 
 .location-box .location {
-  color: #fff;
-  font-size: 32px;
+  color: #FFF;
+  font-size: 65px;
   font-weight: 500;
   text-align: center;
-  text-shadow: 1px 3px rgba(0,0,0,0.25);
+  text-shadow: 3px 3px rgb(50, 50, 70, 0.5);
 }
 
 .location-box .date {
-   color: #fff;
-  font-size: 20px;
+   color: #FFF;
+  font-size: 28px;
   font-weight: 300;
-  text-align: center;
   font-style: italic;
-  text-shadow: 1px 3px rgba(0,0,0,0.25);
+  text-align: center;
+  text-shadow: 2px 2px rgb(50, 50, 70, 0.5);
 }
 
 .weather-box {
@@ -121,24 +171,27 @@ main {
 }
 
 .weather-box .temp {
+   position: relative;
   display: inline-block;
-  padding: 10px 25px;
-  color: #fff;
-  font-size: 102px;
+  margin: 30px auto;
+  background-color: rgb(255, 255, 255, 0.2);
+  border-radius: 16px;
+
+  padding: 15px 25px;
+
+  color: #FFF;
+  font-size: 120px;
   font-weight: 900;
 
-  text-shadow: 3px 6px rgba(0,0,0,0.25);
-  background-color: rgba(255,255,255,0.25);
-  border-radius: 16px;
-  margin: 30px 0px;
-  box-shadow: 3px 6px rgba(0,0,0,0.25);
+  text-shadow: 3px 6px rgb(50, 50, 70, 0.5);
+  text-align: center;
+  box-shadow: 3px 6px rgba(0,0,0,0.2);
 }
 
 .weather-box .weather{
-  color: #fff;
+    color: #FFF;
   font-size: 48px;
   font-weight: 700;
-  font-style: italic;
-  text-shadow: 3px 6px rgba(0,0,0,0.25);
+  text-shadow: 3px 3px rgb(50, 50, 70, 0.5);
 }
 </style>
